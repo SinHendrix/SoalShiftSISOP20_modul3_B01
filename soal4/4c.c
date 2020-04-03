e27c2873cb3c63209be0860b6cb63484224947ad
SinHendrix <56729915+SinHendrix@users.noreply.github.com>
2020-04-03 10:54:16 +0700

    merapikan

--- soal4/4c.c
+++ soal4/4c.c
@@ -4,57 +4,43 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#define READ_END 0
+#define WRITE_END 1
 
-int pid;
-int pipe1[2];
-// int pipe2[2];
+int main(){
+  char firstcmd[100] = "ls", frsarg[100] = "-l", scmd[100] = "wc", secarg[100] = "-l";
+  pid_t pid;
+  int fd[2];
 
-int main()
-{
-  if (pipe(pipe1) == -1)
+  pipe(fd);
+  pid = fork();
+
+  if(pid==0){
+    dup2(fd[WRITE_END], STDOUT_FILENO);
+    close(fd[READ_END]);
+    close(fd[WRITE_END]);
+    execlp(firstcmd, firstcmd, (char*) NULL);
+    printf("Failed to execute '%s'\n", firstcmd);
     exit(1);
-
-  if ((fork()) == 0){
-    // output to pipe1
-    dup2(pipe1[1], 1);
-    // close fds
-    close(pipe1[0]);
-    close(pipe1[1]);
-    // exec
-    char *argv1[] = {"ls", NULL};
-		execv("/bin/ls", argv1);
   }
-
-  // if (pipe(pipe2) == -1)
-  //   exit(1);
-
-  // if (fork() == 0)
-  // {
-  //   // input from pipe1
-  //   dup2(pipe1[0], 0);
-  //   // output to pipe2
-  //   dup2(pipe2[1], 1);
-  //   // close fds
-  //   close(pipe1[0]);
-  //   close(pipe1[1]);
-  //   close(pipe2[0]);
-  //   close(pipe2[1]);
-  //   // exec
-  //   char *argv1[] = {"head", "-n", "3", NULL};
-	// 	execv("/usr/bin/head", argv1);
-  // }
   else{
-    // input from pipe1
-    dup2(pipe1[0], 0);
-    // output to pipe2
-    // dup2(pipe2[1], 1);
-    // close fds
-    close(pipe1[0]);
-    close(pipe1[1]);
-    // exec
-    char *argv1[] = {"wc", "-l", NULL};
-		execv("/usr/bin/wc", argv1);
+    pid=fork();
+
+    if(pid==0)
+    {
+        dup2(fd[READ_END], STDIN_FILENO);
+        close(fd[WRITE_END]);
+        close(fd[READ_END]);
+        execlp(scmd, scmd, secarg,(char*) NULL);
+        printf("Failed to execute '%s'\n", scmd);
+        exit(1);
+    }
+    else
+    {
+        int status;
+        close(fd[READ_END]);
+        close(fd[WRITE_END]);
+        waitpid(pid, &status, 0);
+    }
   }
-  // close(pipe1[0]);
-  // close(pipe1[1]);
 }
