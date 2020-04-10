5d48e55bff903d3c5aeeb67bf85e5c0e181c2b3e
SinHendrix <56729915+SinHendrix@users.noreply.github.com>
2020-04-11 00:10:56 +0700

    Update kategori.c

--- soal3/kategori.c
+++ soal3/kategori.c
@@ -16,9 +16,6 @@
 #include <pthread.h>
 #include <limits.h>
 
-// typedef struct arg_struct {
-//     char asal[1000];
-// }arg_struct;
     
 char cwd[1000];
 
@@ -30,8 +27,6 @@ int is_regular_file(const char *path) //jika 0 bukan file
 }
 
 void pindahFile(char *argv){
-//   printf("stringvoid = %s\n", argv);
-//   printf("stringvoid = %s\n", cwd);
   
   char string[1000];
   strcpy(string, argv);
@@ -60,7 +55,6 @@ void pindahFile(char *argv){
   {
     if(!isFile){
       printf("ini adalah folder, salah argumen\n");
-    //   mkdir(nama, 0777);
       return;
     }
     else
@@ -71,7 +65,6 @@ void pindahFile(char *argv){
     mkdir((tipeLow + 1), 0777); //bikin folder ekstensi
 
     size_t len = 0 ;
-    // strcpy
     char a[1000] ; //res
     strcpy(a, argv);
     char b[1000] ; //des
@@ -90,24 +83,17 @@ void pindahFile(char *argv){
 void *pindahf(void* arg){
   char* asal = (char*) arg;
   printf("asal = %s\n", asal);
-  // printf("stringthr = %s\n", args.cwd);
-  // puts(asal);
   pindahFile(asal);
   pthread_exit(0);
 }
 
 void sortHere(char *asal){
-  // arg_struct args;
-  // args.cwd = "/home/sin/modul3";
-  strcpy(cwd,"/home/sin/modul3/soal3/soal3");
   DIR *dirp;
   char files[240][240];
     struct dirent *entry;
     dirp = opendir(asal);
     int index = 0;
-    // pthread_t tid[1000]; //max 1000 thread
-    // if (d)
-    // {
+
       while ((entry = readdir(dirp)) != NULL)
       {
         if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
@@ -127,9 +113,7 @@ void sortHere(char *asal){
         }
       }
       closedir(dirp);
-    // }
 
-    // chdir(cwd);
     pthread_t threads[index]; 
     for (int i = 0; i < index; i++)
       pthread_create(&threads[i], NULL, pindahf, files[i]);
@@ -139,10 +123,9 @@ void sortHere(char *asal){
 }
 int main(int argc, char* argv[]) 
 { 
-
-  // char cwd[1000];
-  // arg_struct args;
   getcwd(cwd, sizeof(cwd));
+  char asal[120];
+  strcpy(asal, cwd);
 
   
   if(strcmp(argv[1],"-f")==0)//command -f--------------------------------------------------------------
@@ -151,9 +134,8 @@ int main(int argc, char* argv[])
     int index = 0;
     for (int i = 2; i < argc; i++)
     {
-      // strcpy(args.asal, argv[i]);
       pthread_create(&tid[index], NULL, pindahf, argv[i]);
-      sleep(0.5);
+      sleep(0.8);
       index++;
     }
     for (int i = 0; i < index; i++) {
@@ -161,17 +143,13 @@ int main(int argc, char* argv[])
     }
   }
   else if(strcmp(argv[1],"*")==0)
-  {
-    char asal[] = "/home/sin/modul3/soal3/soal3";
-    
+  {    
     sortHere(asal);
   }
   else if(strcmp(argv[1],"-d")==0){
       char asal[1000];
       strcpy(asal, argv[2]);
       sortHere(asal);
-    //   rename(asal, args.cwd);
-    //   sortHere(asal);
   }
   else
   {
